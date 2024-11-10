ca1db8503f01c643fa0a494d9752ce41c14b55f0
nicholas benedetti <nicholas.benedetti@studio.unibo.it>
2024-11-10 20:08:30 +0100

    ex-72: modified MonthSorterNested but still doesn't pass sorting test

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -16,9 +16,47 @@ public final class MonthSorterNested implements MonthSorter {
     private static final int THIRTY = 30;
     private static final int TWENTYEIGHT = 28;
 
-    private static enum Month{
+    private enum Month{
         GENNAIO, FEBBRAIO, MARZO, APRILE, MAGGIO, GIUGNO,   
-        LUGLIO, AGOSTO, SETTEMBRE, OTTOBRE, NOVEMBRE, DICEMBRE
+        LUGLIO, AGOSTO, SETTEMBRE, OTTOBRE, NOVEMBRE, DICEMBRE;
+
+        /**
+         * 
+         * 
+         * @param input
+         * 
+         * @return an enum of Month that most represents the String taken as input
+         */
+        public static Month fromString(String input){
+            Objects.requireNonNull(input);
+            if(input.length() > 9){
+                throw new IllegalArgumentException("No such month with that name");
+            }
+
+            List<String> modifiedMonths = new ArrayList<>();
+            Month[] enuMonths = Month.values();
+            Month result = null;
+            int i = 0;
+
+            for (String month : MONTHS) {
+                if(month.length() >= input.length()){
+                    modifiedMonths.add(month.substring(0, input.length()));
+                }  
+            }    
+            for (String string : modifiedMonths) {
+                if(string.compareToIgnoreCase(input) == 0){
+                    result = enuMonths[i];
+                }
+                i++;
+            }
+            try {
+                return Objects.requireNonNull(result);
+
+            } catch (NullPointerException e) {
+                throw new IllegalArgumentException("No such month with that name", e);
+            }
+            
+        }
     }
 
     private static final ArrayList<String> MONTHS = new ArrayList<>(List.of(
@@ -30,70 +68,30 @@ public final class MonthSorterNested implements MonthSorter {
     static{
         for (Month month : Month.values()) {
             if(month == Month.APRILE || month == Month.NOVEMBRE || month == Month.GIUGNO || month == Month.SETTEMBRE){
-
                 MONTH_WITH_DAYS.put(month, THIRTY);
             } else if(month == Month.FEBBRAIO) {
-
                 MONTH_WITH_DAYS.put(month, TWENTYEIGHT);
             } else {
-                
                 MONTH_WITH_DAYS.put(month, THIRTYONE);
             }
-            
         }
     }
 
-    /**
-     * 
-     * 
-     * @param input
-     * 
-     * @return an enum of Month that most represents the String taken as input
-     */
-    public Month fromString(String input){
-        Objects.requireNonNull(input);
-
-        if(input.length() > 9){
-            throw new IllegalArgumentException("No such month with that name");
-        }
-        List<String> modifiedMonths = new ArrayList<>();
-        Month[] enuMonths = Month.values();
-        int i = 0;
-        for (String month : MONTHS) {
-            if(month.length() >= input.length()){
-                modifiedMonths.add(month.substring(0, input.length()));
-            }  
-        }
-        for (String string : modifiedMonths) {
-            if(string.compareToIgnoreCase(input) == 0){
-                return enuMonths[i];
-            }
-            i++;
-        }
-        
-        throw new IllegalArgumentException("No such month with that name");
-    } 
-
-    public class SortByMonthOrder implements Comparator<String> {
+    private static final class SortByMonthOrder implements Comparator<String> {
 
         @Override
         public int compare(String string1, String string2) {
             
-            return fromString(string1).compareTo(fromString(string2));
+            return Month.fromString(string1).compareTo(Month.fromString(string2));
         }
     }
 
-    public class SortByDate implements Comparator<String> {
-        private Month input1 = null;
-        private Month input2 = null;
+    private static final class SortByDate implements Comparator<String> {
 
         @Override
         public int compare(String string1, String string2) {
-            input1 = fromString(string1);
-            input2 = fromString(string2);
-            
-            int daysOfString1 = MONTH_WITH_DAYS.get(input1);
-            int daysOfString2 = MONTH_WITH_DAYS.get(input2);
+            int daysOfString1 = MONTH_WITH_DAYS.get(Month.fromString(string1));
+            int daysOfString2 = MONTH_WITH_DAYS.get(Month.fromString(string2));
 
             return Integer.compare(daysOfString1, daysOfString2);            
         }
